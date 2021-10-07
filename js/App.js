1db812323852ff33e4d848f6278431a3f3b1eccc
Iñaki García <yarimfuego@gmail.com>
2021-10-07 01:49:22 -0500

    Agregado efecto de escritura

--- js/App.js
+++ js/App.js
@@ -21,3 +21,16 @@ botonMenu.addEventListener( 'click', e => {
     }
 
 });
+
+// Efecto escritura - Typed
+const typed = new Typed('.typed', {
+    // Configuración del efecto
+    strings: [ 'Aplicaciones Web', 'Aplicaciones Moviles', 'Videojuegos', 'Páginas Web' ],
+    typeSpeed: 60,
+    startDelay: 150,
+    backSpeed: 35,
+    backDelay: 1000,
+    loop: true,
+    smartBackspace: true,
+    loopCount: false
+});
